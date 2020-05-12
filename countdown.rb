b9ebae0ff7e1eee3b86463a83f1f2776615c6d00
Nguyen Ros <nynha0526@gmail.com>
2020-05-12 01:41:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,7 @@
 #write your code here
 
-def countdown
-  counter = 10
-  while counter >= 0 
+def countdown(counter)
+  while counter > 0 
     puts "#{counter} SECOND(S)!"
     counter -= 1
   end
